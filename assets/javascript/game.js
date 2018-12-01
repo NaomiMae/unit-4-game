5300adbb9d296c04a7610d8c9b89ebf880bd0d72
Naomi Johnson <naomikaiena@gmail.com>
2018-11-30 21:50:30 -0500

    Final Crystal Game without README

---
+++ assets/javascript/game.js
@@ -0,0 +1,116 @@
+$(document).ready(function() { 
+var purpleCrystal = $("#purple");
+var greenCrystal = $("#green");
+var pinkCrystal = $("#pink");
+var yellowCrystal = $("#yellow");
+var randomNumber = $("#randomNumber");
+var totalWins = $("#totalWins");
+var totalLoses = $("#totalLoses");
+var totalScore = $("#totalScore");
+
+
+var addingNumbers = 0;
+var crystalNumber =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var wins = 0;
+var loses = 0;
+// var targetNumber = []
+
+var randomNumberGenerated = randomRange(19, 120);
+randomNumber.text(randomNumberGenerated);
+// document.getElementById("randomNumber").style.fontSize = "xx-large";
+$("#randomNumber").text('Target Score: ' + randomNumberGenerated);
+$("#totalScore").text('Your Total Score is: ' + "0") 
+
+
+
+function setGame (){
+    purpleNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    greenNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    pinkNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    yellowNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    randomNumberGenerated = randomRange(19, 120);
+    function randomRange (myMin, myMax){
+        return Math.floor(Math.random() * (myMax - myMin +1)) + myMin ;
+    }
+    $("#randomNumber").text('Target Score: ' + randomNumberGenerated);
+    addingNumbers = 0; 
+    $("#totalScore").text('Your Total Score is: ' + "0") 
+}
+
+var purpleNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+var greenNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+var pinkNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+var yellowNumbers = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+
+// calling out the random number and sizing it
+
+function randomRange (myMin, myMax){
+    return Math.floor(Math.random() * (myMax - myMin +1)) + myMin ;
+    // console.log
+}
+
+  
+
+// Giving the crystals their numbers
+
+purpleCrystal.on("click", function() {
+    addingNumbers += purpleNumbers;
+    console.log(addingNumbers);
+    $("#totalScore").text('Your Total Score is: ' + addingNumbers);
+    checkWin();
+});
+
+greenCrystal.on ("click", function() {
+    addingNumbers += greenNumbers;
+    console.log(addingNumbers);
+    $("#totalScore").text('Your Total Score is: ' + addingNumbers);
+    checkWin();
+});
+
+pinkCrystal.on ("click" , function() {
+    addingNumbers += pinkNumbers;
+    $("#totalScore").text('Your Total Score is: ' + addingNumbers);
+    checkWin();
+
+});
+yellowCrystal.on ("click" , function() {
+    addingNumbers += yellowNumbers;
+    $("#totalScore").text('Your Total Score is: ' + addingNumbers);
+    checkWin();
+
+});
+
+// checking to see if you win or lose
+
+function checkWin (){
+
+    if (addingNumbers === randomNumberGenerated){
+        wins ++;
+        // alert("You Win");
+        // $("totalWins").text("Wins:" + wins);
+        totalWins.text ("Wins:" + wins);
+        // addingNumbers = 0;
+        // $("#randomNumber").text('Target Score: ' + randomNumberGenerated);
+        setGame();
+    }
+    else if (addingNumbers > randomNumberGenerated){
+        loses ++;
+        // alert("You Lose"); 
+        totalLoses.text ("Loses: " + loses); 
+        // addingNumbers = 0; 
+        // $("#randomNumber").text('Target Score: ' + randomNumberGenerated);
+        setGame();   
+    }
+    
+}
+
+// function setGame (myMin, myMax){
+//         return Math.floor(Math.random() * (myMax - myMin +1)) + myMin ;
+//         console.log
+// randomNumber.textContent = randomRange;
+// }
+
+
+// updateDom( )
+
+});
